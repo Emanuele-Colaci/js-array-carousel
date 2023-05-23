b5f889fb15f6fb0484b1ba9e1b1642ad0f2bd2de
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-05-23 15:58:22 +0200

    Creato array,  variabili e ciclo for

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//ARRAY
+const images = [
+    './img/01.webp',
+    './img/02.webp',
+    './img/03.webp',
+    './img/04.webp',
+    './img/05.webp'
+];
+
+//VARIABILI
+const imgGroup = document.querySelector('.group-img');
+const avanti = document.querySelector('.avanti');
+const indietro = document.querySelector('.indietro');
+const img = document.createElement("img", "slide");
+const dBlock = document.createElement("d-block");
+
+for(i = 0; i < images.length; i++){
+
+    if()
+}
